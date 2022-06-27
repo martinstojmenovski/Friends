485be10a6b8361486fe921a6aefdb0e0df27f841
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2022-06-27 17:02:18 -0600

    Form added in react

--- frontend/frontendapp/src/App.js
+++ frontend/frontendapp/src/App.js
@@ -1,10 +1,9 @@
-
 import './App.css';
-
+import Login from './components/login';
 function App() {
   return (
     <div className="App">
-      <h1>Login user</h1>
+      <Login />
     </div>
   );
 }
