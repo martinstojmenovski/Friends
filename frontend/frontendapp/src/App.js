ad5042d00a0262e511d60fb9e3164e95f4f6b37f
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2022-06-27 18:02:10 -0600

    end of the day I am working on the routes

--- frontend/frontendapp/src/App.js
+++ frontend/frontendapp/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
-import Login from './components/login';
+import Login from './components/Login';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-      <Login />
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/'>Login</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+
     </div>
   );
 }
